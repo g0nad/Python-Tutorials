017d5ad1d4529ebbba7fec59882692335a6d4593
James Payne <payne.james@gmail.com>
2020-09-25 23:52:43 -0400

    new variables

---
+++ variables.py
@@ -0,0 +1,22 @@
+# bob = 17
+# rob = 18
+# don = 19
+
+# print(bob)
+# print(rob)
+# print(don)
+
+# bob = rob = don = 17
+# print(bob, rob, don)
+
+# print(bob + rob)
+# print(don * bob)
+# print(bob - don)
+
+
+first_name = "James"
+last_name = "Payne"
+age = 44
+sex = "Male"
+
+print((first_name + " " + last_name) * 10)
