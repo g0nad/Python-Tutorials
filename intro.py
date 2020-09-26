53b397a88f65c68133902cfc20bd88c19ff564e5
James Payne <payne.james@gmail.com>
2020-09-26 00:01:27 -0400

    playing with variables

--- intro.py
+++ intro.py
@@ -1,39 +1,39 @@
 # Variables Defined
 
 # Basic Variable
-intro_message = "hello world"
+# intro_message = "hello world"
 
 # Basic variable with '
-intro_message_2 = "hello's world"
+# intro_message_2 = "hello's world"
 
 # Variable with multiple lines
 
 intro_message_3 = """jimmy as a really really really
 good boy he was"""
 
-
 # Print
 
 # Print Welcome Message
-print(intro_message)
+# print(intro_message)
 
 # Print basic variable with '
-print(intro_message_2)
+# print(intro_message_2)
 
 # Print basic varaible multiple lines
-print(intro_message_3)
+# print(intro_message_3)
 
 # This prints the length of the variable
-print(len(intro_message_3))
+# print(len(intro_message_3))
 
 # This prints the first character of the variable, starting at 0
-print(intro_message_3[0])
+print(intro_message_3[:-5])
 
 # This prints the first five character of the variable, starting at 0, 1, 2, 3 (give me 0 up to 5 so 0 1 2 3 4 )
-print(intro_message_3[0:5])
+# print(intro_message_3[0:5])
+# print(intro_message_3)[:-1]
 
 # This gives me 6 to the end
-print(intro_message_3[6:])
+# print(intro_message_3[6:])
 
 # This gives me the last 6
-print(intro_message_3[:6])
+# print(intro_message_3[:6])
